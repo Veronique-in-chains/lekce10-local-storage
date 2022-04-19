8692bbf6ff24c9ae2dc4f8fd7ca7562fcf835719
Veronika Bosáková <veronikabosakova@gmail.com>
2022-04-19 19:40:19 +0200

    Functionality added.

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
 // do proměnné si uložíme prvek ve stránce,
 // kam budeme vypisovat výsledek
-
+const vypis = document.querySelector("#vysledek");
 // ihned po načtení stránky zavoláme funkci,
 // která přečte hodnotu z Local Storage a zobrazí ji
-
+nactiHodnotu();
 // funkce pro načtení a zobrazení hodnoty z Local Storage
 function nactiHodnotu() {
 	// načteme z Local Storage
-
+  let hodnota = localStorage.mojeHodnota;
 	// pokud hodnota nexistuje, vypíšeme chybu
-		
+		if (hodnota === null || hodnota === undefined) {
+      vypis.textContent = "Není zadána žádná hodnota. "
+    } else {
 	// jinak hodnota existuje a vypíšeme ji
-
+      vypis.textContent = hodnota;
+    }
 }
 
 // funkce pro uložení hodnoty do Local Storage
 function ulozHodnotu() {
 	// hodnotu načteme z pole formuláře
-
+  let mojeHodnota = document.querySelector("#hodnota").value;
 	// a uložíme do local storage pod jménem "mojehodnota"
-
+  localStorage.mojeHodnota = JSON.stringify(mojeHodnota);
 	// a hned zavoláme funkci pro zobrazení hodnoty,
 	// aby se změna v Local Storage promítla i na stránce
-
+  nactiHodnotu();
 }
 
 // funkce pro smazání hodnoty z Local Storage
 function smazatHodnotu() {
 	// smažeme hodnotu uloženou pod jménem "mojehodnota",
 	// pokud takový klíč neexistuje, nestane se nic
-
+  localStorage.removeItem("mojeHodnota");
 	// a hned zavoláme funkci pro zobrazení hodnoty,
 	// aby se změna v Local Storage promítla i na stránce
-
+  nactiHodnotu();
 }
